9d3b82cad294ef7e39e93ca279c87cdfbd314039
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-05 16:46:08 +0200

    + jwt module configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from 'src/users/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersService],
+  imports: [
+    UsersService,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_AUTH'),
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
