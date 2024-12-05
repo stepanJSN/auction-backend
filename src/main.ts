e55f3a4b881ae6792b17cfaa5c57660daaf261f6
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-05 16:31:18 +0200

    + main configuration and all dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  const clientUrl = configService.get<string>(
+    'CLIENT_URL',
+    'http://localhost:5000',
+  );
+
+  app.enableCors({
+    origin: [clientUrl],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+  await app.listen(port);
 }
 bootstrap();
