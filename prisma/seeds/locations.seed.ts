e132e786360b1979d654f5558dccab19b5acd9c9
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-05 10:10:40 +0200

    + field id to locations seed

--- prisma/seeds/locations.seed.ts
+++ prisma/seeds/locations.seed.ts
@@ -2,14 +2,16 @@ import { prismaInstance } from '../seed';
 import { getData } from './getData';
 
 type LocationType = {
+  id: number;
   name: string;
   type: string;
 };
 
-export async function seedLocation() {
+export async function seedLocations() {
   const locationsUrl = process.env.API_URL + 'location';
   const allLocations = await getData<LocationType>(locationsUrl);
   const mappedLocations = allLocations.map((location) => ({
+    id: location.id,
     name: location.name,
     type: location.type,
   }));
