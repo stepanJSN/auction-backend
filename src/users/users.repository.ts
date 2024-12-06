1f4bd303a9b6b36e2dabc1b6874d8fa3b81b16ce
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-06 11:12:15 +0200

    moved methods for working with the database to repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserType } from './types/createUser.type';
 
 @Injectable()
 export class UsersRepository {
@@ -15,6 +16,32 @@ export class UsersRepository {
         password: true,
         role: true,
       },
+    });
+  }
+
+  async create(createUsersDto: CreateUserType) {
+    const { id } = await this.prisma.users.create({
+      data: {
+        email: createUsersDto.email,
+        name: createUsersDto.name,
+        surname: createUsersDto.surname,
+        password: createUsersDto.password,
+      },
+    });
+    return id;
+  }
+
+  findAll(page: number, take: number) {
+    return this.prisma.users.findMany({
+      select: {
+        id: true,
+        name: true,
+        surname: true,
+        rating: true,
+        role: true,
+      },
+      skip: (page - 1) * take,
+      take,
     });
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { CreateUsersDto } from './dto/create-users.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private usersRepository: UsersRepository) {}
 
+  private async hashPassword(password: string) {
+    const saltRounds = 10;
+    return await hash(password, saltRounds);
+  }
+
+  async create(createUsersDto: CreateUsersDto) {
+    const user = await this.findOneByEmail(createUsersDto.email);
+
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    return await this.usersRepository.create({
+      email: createUsersDto.email,
+      name: createUsersDto.name,
+      surname: createUsersDto.surname,
+      password: await this.hashPassword(createUsersDto.password),
+    });
+  }
+
   findOneByEmail(email: string) {
     return this.usersRepository.findOneByEmail(email);
+  }
+
+  findAll(page = 1, take = 10) {
+    return this.usersRepository.findAll(page, take);
   }
 }
