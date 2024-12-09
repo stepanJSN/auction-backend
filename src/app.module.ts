b1e652e346b4e65e0fe0ba402401a4f7f8454043
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-09 13:36:49 +0200

    inject cards module into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { UsersModule } from './users/users.module';
 import { LocationsModule } from './locations/locations.module';
 import { EpisodesModule } from './episodes/episodes.module';
 import { EpisodesCardsModule } from './episodes-cards/episodes-cards.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule, LocationsModule, EpisodesModule, EpisodesCardsModule],
+  imports: [AuthModule, PrismaModule, UsersModule, LocationsModule, EpisodesModule, EpisodesCardsModule, CardsModule],
 })
 export class AppModule {}
