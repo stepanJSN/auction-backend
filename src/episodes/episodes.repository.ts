1bdb15859e66d84ed004d715b6902b9ba307541e
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-09 11:08:12 +0200

    + create, findOne, update, delete methods in repository

--- src/episodes/episodes.repository.ts
+++ src/episodes/episodes.repository.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FindAllEpisodesDto } from './dto/find-all-episodes.dto';
+import { CreateEpisodeDto } from './dto/create-episode.dto';
+import { UpdateEpisodeDto } from './dto/update-episode.dto';
 
 @Injectable()
 export class EpisodesRepository {
   constructor(private prisma: PrismaService) {}
+
+  create(createEpisodeDto: CreateEpisodeDto) {
+    return this.prisma.episodes.create({
+      data: createEpisodeDto,
+    });
+  }
 
   findAll({ name, page = 1, take = 10 }: FindAllEpisodesDto) {
     return this.prisma.episodes.findMany({
@@ -16,5 +24,24 @@ export class EpisodesRepository {
       skip: (page - 1) * take,
       take,
     });
+  }
+
+  findOne(id: number) {
+    return this.prisma.episodes.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  update(id: number, updateEpisodeDto: UpdateEpisodeDto) {
+    return this.prisma.episodes.update({
+      where: { id },
+      data: updateEpisodeDto,
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.episodes.delete({ where: { id } });
   }
 }
