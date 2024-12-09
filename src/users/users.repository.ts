2097399c849d6a4e65fbaf4a64ddc2880fa290e8
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-09 10:33:59 +0200

    renamed create user type

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateUserType } from './types/update-user.type';
-import { CreateUserType } from './types/createUser.type';
+import { CreateUserType } from './types/create-user.type';
 
 @Injectable()
 export class UsersRepository {
