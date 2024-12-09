df4e310b0e766780f927a34e17e5d1fbcb98c87d
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-09 17:18:49 +0200

    - episode cards module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { LocationsModule } from './locations/locations.module';
 import { EpisodesModule } from './episodes/episodes.module';
-import { EpisodesCardsModule } from './episodes-cards/episodes-cards.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule, LocationsModule, EpisodesModule, EpisodesCardsModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    UsersModule,
+    LocationsModule,
+    EpisodesModule,
+  ],
 })
 export class AppModule {}
--- src/episodes-cards/types/create-episode-card.type.ts
+++
@@ -1,4 +0,0 @@
-export type CreateEpisodeCardType = {
-  episode_id: number;
-  card_id: string;
-};
