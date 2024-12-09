2f040f8ce93c12cd9cb3176daa2293149941226e
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-09 11:21:36 +0200

    + create, findOne, update, remove endpoints

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,13 +1,56 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { FindAllEpisodesDto } from './dto/find-all-episodes.dto';
+import { RoleGuard } from 'src/guards/role.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+import { CreateEpisodeDto } from './dto/create-episode.dto';
+import { UpdateEpisodeDto } from './dto/update-episode.dto';
 
 @Controller('episodes')
+@UseGuards(RoleGuard)
 export class EpisodesController {
   constructor(private readonly episodesService: EpisodesService) {}
+
+  @Post()
+  @Roles(Role.Admin)
+  create(@Body() createLocationDto: CreateEpisodeDto) {
+    return this.episodesService.create(createLocationDto);
+  }
 
   @Get()
   findAll(@Query() findAllLocations: FindAllEpisodesDto) {
     return this.episodesService.findAll(findAllLocations);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.episodesService.findOne(id);
+  }
+
+  @Patch(':id')
+  @Roles(Role.Admin)
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateLocationDto: UpdateEpisodeDto,
+  ) {
+    return this.episodesService.update(id, updateLocationDto);
+  }
+
+  @Delete(':id')
+  @Roles(Role.Admin)
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.episodesService.remove(id);
   }
 }
