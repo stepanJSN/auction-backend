f72fcf197c675827f49f69e7224d78b01bc495a1
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-09 13:48:02 +0200

    added users-cards module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,9 @@ import { LocationsModule } from './locations/locations.module';
 import { EpisodesModule } from './episodes/episodes.module';
 import { EpisodesCardsModule } from './episodes-cards/episodes-cards.module';
 import { CardsModule } from './cards/cards.module';
+import { UsersCardsModule } from './users-cards/users-cards.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule, LocationsModule, EpisodesModule, EpisodesCardsModule, CardsModule],
+  imports: [AuthModule, PrismaModule, UsersModule, LocationsModule, EpisodesModule, EpisodesCardsModule, CardsModule, UsersCardsModule],
 })
 export class AppModule {}
