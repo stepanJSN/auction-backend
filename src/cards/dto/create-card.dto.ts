52fa3d3d5da891251f6f1a049130309e18f07118
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-10 17:10:59 +0200

    request data transformation in create dto

--- src/cards/dto/create-card.dto.ts
+++ src/cards/dto/create-card.dto.ts
@@ -1,4 +1,5 @@
 import { Gender } from '@prisma/client';
+import { Transform } from 'class-transformer';
 import { IsArray, IsIn, IsInt, IsString, Length } from 'class-validator';
 
 export class CreateCardDto {
@@ -10,12 +11,14 @@ export class CreateCardDto {
   @Length(2, 20)
   type: string;
 
+  @Transform(({ value }) => parseInt(value, 10))
   @IsInt()
   locationId: number;
 
   @IsIn(['unknown', 'female', 'male', 'unknown'])
   gender: Gender;
 
+  @Transform(({ value }) => JSON.parse(value))
   @IsArray()
   episodesId: number[];
 }
