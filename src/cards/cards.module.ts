496802f5983b924555c0dac42e798d86a9fd3377
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-10 17:11:22 +0200

    external imports

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CardsController } from './cards.controller';
 import { CardsRepository } from './cards.repository';
+import { CardInstancesModule } from 'src/card-instances/card-instances.module';
+import { ImagesModule } from 'src/images/images.module';
 
 @Module({
+  imports: [CardInstancesModule, ImagesModule],
   controllers: [CardsController],
   providers: [CardsService, CardsRepository],
 })
