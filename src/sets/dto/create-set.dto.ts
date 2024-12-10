ba6f91bbe3ed787e037e5be1d087bcf1eadc85ce
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-10 17:46:48 +0200

    added create/update dto

--- src/sets/dto/create-set.dto.ts
+++ src/sets/dto/create-set.dto.ts
@@ -1,1 +1,13 @@
-export class CreateSetDto {}
+import { IsArray, IsInt, IsString, Length } from 'class-validator';
+
+export class CreateSetDto {
+  @IsString()
+  @Length(2, 20)
+  name: string;
+
+  @IsInt()
+  bonus: number;
+
+  @IsArray()
+  cardsId: string[];
+}
