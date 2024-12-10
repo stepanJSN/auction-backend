55d7086762879375fb793956750b722ec1c70480
Stepan Stadniuk <stepan.stadniuk.jsn@gmail.com>
2024-12-10 17:12:23 +0200

    added card controller

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,34 +1,78 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+  ParseUUIDPipe,
+  Query,
+} from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FindAllCards } from './dto/find-all-cards.dto';
+import { CurrentUser } from 'src/decorators/user.decorator';
+import { JWTPayload } from 'src/auth/types/auth.type';
 
 @Controller('cards')
 export class CardsController {
   constructor(private readonly cardsService: CardsService) {}
 
   @Post()
-  create(@Body() createCardDto: CreateCardDto) {
-    return this.cardsService.create(createCardDto);
+  @UseInterceptors(FileInterceptor('image'))
+  create(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1000000 }),
+          new FileTypeValidator({
+            fileType: /(image\/(jpeg|png|avif|webp|jpg))/,
+          }),
+        ],
+      }),
+    )
+    image: Express.Multer.File,
+    @Body() createCardDto: CreateCardDto,
+  ) {
+    return this.cardsService.create(createCardDto, image);
   }
 
   @Get()
-  findAll() {
-    return this.cardsService.findAll();
+  findAll(
+    @CurrentUser() user: JWTPayload,
+    @Query() { page, take }: FindAllCards,
+  ) {
+    return this.cardsService.findAll({
+      userId: user.id,
+      role: user.role,
+      page: page ?? 1,
+      take: take ?? 20,
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cardsService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.cardsService.findOne(id, true);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCardDto: UpdateCardDto) {
-    return this.cardsService.update(+id, updateCardDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCardDto: UpdateCardDto,
+  ) {
+    return this.cardsService.update(id, updateCardDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.cardsService.remove(id);
   }
 }
