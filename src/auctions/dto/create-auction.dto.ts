20970054772a4a53695b66ce150d530db6a808be
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-11 16:39:16 +0200

    added find all auction dto and create auction dto

--- src/auctions/dto/create-auction.dto.ts
+++ src/auctions/dto/create-auction.dto.ts
@@ -1,1 +1,24 @@
-export class CreateAuctionDto {}
+import { IsDecimal, IsUUID } from 'class-validator';
+
+export class CreateAuctionDto {
+  @IsUUID()
+  cardInstanceId: string;
+
+  @IsDecimal()
+  starting_bid: number;
+
+  @IsDecimal()
+  min_bid_step: number;
+
+  @IsDecimal()
+  max_bid: number;
+
+  @IsInt()
+  min_length: number;
+
+  @IsInt()
+  max_length: Date;
+
+  @IsUUID()
+  createdBy: string;
+}
---
+++ src/auctions/dto/find-all-auction.dto.ts
@@ -0,0 +1,43 @@
+import { Transform } from 'class-transformer';
+import {
+  IsDecimal,
+  IsIn,
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class FindAllAuctionsDto {
+  @IsOptional()
+  @IsInt()
+  locationId: number;
+
+  @IsOptional()
+  @IsString()
+  cardName: string;
+
+  @IsOptional()
+  @IsDecimal()
+  fromPrice: number;
+
+  @IsOptional()
+  @IsDecimal()
+  toPrice: number;
+
+  @IsOptional()
+  @IsIn(['asc', 'desc'])
+  sortOrder: 'asc' | 'desc';
+
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+  @Min(1)
+  @Max(50)
+  take?: number;
+
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+  @Min(1)
+  page?: number;
+}
