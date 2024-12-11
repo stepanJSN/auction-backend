242c0f6ce9847298d77aa0cc3dc37148ddf35c93
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-11 17:00:40 +0200

    added method for creating new card instances

--- src/card-instances/card-instances.service.ts
+++ src/card-instances/card-instances.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CardInstancesRepository } from './card-instances.repository';
 import { FindAllCardInstancesType } from './types/find-all-card-instances.type';
+import { CreateCardInstanceType } from './types/create-card-instance.type';
 
 @Injectable()
 export class CardInstancesService {
   constructor(private cardInstancesRepository: CardInstancesRepository) {}
+
+  create(cardInstanceData: CreateCardInstanceType) {
+    return this.cardInstancesRepository.create(cardInstanceData);
+  }
 
   findAll(findAllCardInstances: FindAllCardInstancesType) {
     return this.cardInstancesRepository.findAll(findAllCardInstances);
