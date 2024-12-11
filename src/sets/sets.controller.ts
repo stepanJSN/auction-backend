9d5835d4aae85f8d4e65e53d9aeba105369d5676
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-11 10:32:51 +0200

    added sets controller

--- src/sets/sets.controller.ts
+++ src/sets/sets.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { SetsService } from './sets.service';
 import { CreateSetDto } from './dto/create-set.dto';
 import { UpdateSetDto } from './dto/update-set.dto';
+import { FindAllSets } from './dto/find-all-sets.dto';
+import { CurrentUser } from 'src/decorators/user.decorator';
+import { JWTPayload } from 'src/auth/types/auth.type';
 
 @Controller('sets')
 export class SetsController {
@@ -13,22 +26,28 @@ export class SetsController {
   }
 
   @Get()
-  findAll() {
-    return this.setsService.findAll();
+  findAll(
+    @CurrentUser() { role, id }: JWTPayload,
+    @Query() { page, take }: FindAllSets,
+  ) {
+    return this.setsService.findAll({ role, userId: id, page, take });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.setsService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.setsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSetDto: UpdateSetDto) {
-    return this.setsService.update(+id, updateSetDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateSetDto: UpdateSetDto,
+  ) {
+    return this.setsService.update(id, updateSetDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.setsService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.setsService.remove(id);
   }
 }
