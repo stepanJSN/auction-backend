cdc5ec139f75207ba4b1451f4a7b2dc1e2bdb964
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-12 09:22:27 +0200

    + auction service

--- src/auctions/auctions.service.ts
+++ src/auctions/auctions.service.ts
@@ -1,26 +1,75 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuctionDto } from './dto/create-auction.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UpdateAuctionDto } from './dto/update-auction.dto';
+import { AuctionsRepository } from './auctions.repository';
+import { CardInstancesService } from 'src/card-instances/card-instances.service';
+import { CreateAuctionServiceType } from './types/create-auction-service.type';
+import { FindAllAuctionsDto } from './dto/find-all-auction.dto';
 
 @Injectable()
 export class AuctionsService {
-  create(createAuctionDto: CreateAuctionDto) {
-    return 'This action adds a new auction';
+  constructor(
+    private auctionRepository: AuctionsRepository,
+    private cardInstancesService: CardInstancesService,
+  ) {}
+
+  async create(createAuctionDto: CreateAuctionServiceType) {
+    if (createAuctionDto.role === 'Admin') {
+      const { id: cardInstanceId } = await this.cardInstancesService.create({
+        userId: createAuctionDto.createdBy,
+        cardId: createAuctionDto.cardId,
+      });
+      return this.auctionRepository.create({
+        ...createAuctionDto,
+        cardInstanceId,
+      });
+    }
+
+    const cardInstance = await this.cardInstancesService
+      .findAll({
+        userId: createAuctionDto.createdBy,
+        cardsId: [createAuctionDto.cardId],
+      })
+      .then((cardInstances) => cardInstances.pop());
+
+    if (!cardInstance) {
+      throw new BadRequestException("You don't have this card");
+    }
+
+    return this.auctionRepository.create({
+      ...createAuctionDto,
+      cardInstanceId: cardInstance.id,
+    });
   }
 
-  findAll() {
-    return `This action returns all auctions`;
+  findAll(findAllAuctionsDto: FindAllAuctionsDto) {
+    return this.auctionRepository.findAll(findAllAuctionsDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auction`;
+  async findOne(id: string, userId: string) {
+    const { bids, card_instance, ...restAuctionData } =
+      await this.auctionRepository.findOne(id);
+    const highestBid = bids.sort(
+      (bidA, bidB) => +bidB.bid_amount - +bidA.bid_amount,
+    )[0];
+    return {
+      ...restAuctionData,
+      card: {
+        ...card_instance.cards,
+      },
+      bid: {
+        amount: highestBid.bid_amount,
+        isThisUserBid: highestBid.user_id === userId,
+      },
+    };
   }
 
-  update(id: number, updateAuctionDto: UpdateAuctionDto) {
-    return `This action updates a #${id} auction`;
+  async update(id: string, updateAuctionDto: UpdateAuctionDto) {
+    await this.auctionRepository.findOne(id);
+    return this.auctionRepository.update(id, updateAuctionDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auction`;
+  async remove(id: string) {
+    await this.auctionRepository.findOne(id);
+    return this.auctionRepository.remove(id);
   }
 }
