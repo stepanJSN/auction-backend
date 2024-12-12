6897a3e21c44f22148c82fd3397827b72d0b8127
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-12 11:58:23 +0200

    updated module dependencies and providers

--- src/auctions/auctions.module.ts
+++ src/auctions/auctions.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuctionsService } from './auctions.service';
 import { AuctionsController } from './auctions.controller';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AuctionsRepository } from './auctions.repository';
+import { AuctionsGateway } from './auctions.gateway';
+import { CardInstancesModule } from 'src/card-instances/card-instances.module';
 
 @Module({
+  imports: [
+    EventEmitterModule.forRoot(),
+    ScheduleModule.forRoot(),
+    CardInstancesModule,
+  ],
   controllers: [AuctionsController],
-  providers: [AuctionsService],
+  providers: [AuctionsService, AuctionsRepository, AuctionsGateway],
 })
 export class AuctionsModule {}
