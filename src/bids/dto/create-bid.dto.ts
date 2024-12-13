ed41468e39f65969ca4e6a29f66c5db9e6c0ab1c
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-13 11:37:00 +0200

    removed update dto and updated create dto

--- src/bids/dto/create-bid.dto.ts
+++ src/bids/dto/create-bid.dto.ts
@@ -1,1 +1,9 @@
-export class CreateBidDto {}
+import { IsInt, IsUUID } from 'class-validator';
+
+export class CreateBidDto {
+  @IsUUID()
+  auctionId: string;
+
+  @IsInt()
+  bidAmount: number;
+}
