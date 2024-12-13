10e96e4eb6185e1d7ef8a858f8220edf9e2e763c
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-13 11:54:14 +0200

    + bids repository

--- src/bids/bids.module.ts
+++ src/bids/bids.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BidsService } from './bids.service';
 import { BidsController } from './bids.controller';
+import { BidsRepository } from './bids.repository';
 
 @Module({
   controllers: [BidsController],
-  providers: [BidsService],
+  providers: [BidsService, BidsRepository],
 })
 export class BidsModule {}
