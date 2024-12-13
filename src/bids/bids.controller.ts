b8c0e14aa9a4b6e7caed76f947cb7f84c430b9dd
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-13 16:59:10 +0200

    bids controller

--- src/bids/bids.controller.ts
+++ src/bids/bids.controller.ts
@@ -1,34 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { BidsService } from './bids.service';
 import { CreateBidDto } from './dto/create-bid.dto';
-import { UpdateBidDto } from './dto/update-bid.dto';
+import { CurrentUser } from 'src/decorators/user.decorator';
 
 @Controller('bids')
 export class BidsController {
   constructor(private readonly bidsService: BidsService) {}
 
   @Post()
-  create(@Body() createBidDto: CreateBidDto) {
-    return this.bidsService.create(createBidDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.bidsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.bidsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBidDto: UpdateBidDto) {
-    return this.bidsService.update(+id, updateBidDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bidsService.remove(+id);
+  create(
+    @CurrentUser('id') userId: string,
+    @Body() createBidDto: CreateBidDto,
+  ) {
+    return this.bidsService.create({ userId, ...createBidDto });
   }
 }
--- src/bids/bids.module.ts
+++ src/bids/bids.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { BidsService } from './bids.service';
 import { BidsController } from './bids.controller';
 import { BidsRepository } from './bids.repository';
+import { AuctionsModule } from 'src/auctions/auctions.module';
 
 @Module({
+  imports: [AuctionsModule],
   controllers: [BidsController],
   providers: [BidsService, BidsRepository],
 })
