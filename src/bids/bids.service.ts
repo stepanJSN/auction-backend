f7ba033c361933b8db5107db9faf122785d6d50d
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-13 12:50:55 +0200

    added bid service and event

--- src/bids/bids.service.ts
+++ src/bids/bids.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
-import { CreateBidDto } from './dto/create-bid.dto';
-import { UpdateBidDto } from './dto/update-bid.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { BidsRepository } from './bids.repository';
+import { CreateBidType } from './types/create-bid.type';
+import { AuctionsService } from 'src/auctions/auctions.service';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { NewBidEvent } from './events/new-bid.event';
 
 @Injectable()
 export class BidsService {
-  create(createBidDto: CreateBidDto) {
-    return 'This action adds a new bid';
-  }
+  constructor(
+    private bidsRepository: BidsRepository,
+    private auctionsService: AuctionsService,
+    private eventEmitter: EventEmitter2,
+  ) {}
 
-  findAll() {
-    return `This action returns all bids`;
-  }
+  async create(createBidData: CreateBidType) {
+    const auction = await this.auctionsService.findOne(
+      createBidData.auctionId,
+      createBidData.userId,
+    );
 
-  findOne(id: number) {
-    return `This action returns a #${id} bid`;
-  }
+    if (auction.is_completed) {
+      throw new BadRequestException('The auction has already ended!');
+    }
 
-  update(id: number, updateBidDto: UpdateBidDto) {
-    return `This action updates a #${id} bid`;
-  }
+    if (auction.card.isUserHasThisCard) {
+      throw new BadRequestException('You already have this card!');
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} bid`;
+    if (
+      !auction.highestBid.amount &&
+      auction.starting_bid > createBidData.bidAmount
+    ) {
+      throw new BadRequestException('You cannot bid less than starting bid!');
+    }
+
+    if (
+      auction.highestBid.amount &&
+      auction.highestBid.amount >= createBidData.bidAmount
+    ) {
+      throw new BadRequestException(
+        'You cannot bid less than your previous bid!',
+      );
+    }
+
+    if (auction.max_bid && auction.max_bid < createBidData.bidAmount) {
+      throw new BadRequestException('Your bid exceeds the maximum allowed!');
+    }
+
+    if (
+      auction.min_bid_step <=
+      createBidData.bidAmount - auction.highestBid.amount
+    ) {
+      throw new BadRequestException(
+        'Your bid does not exceed the minimum bid step!',
+      );
+    }
+
+    const { created_at, bid_amount, auction_id } =
+      await this.bidsRepository.create(createBidData);
+    this.eventEmitter.emit(
+      'bid.new',
+      new NewBidEvent({
+        createdAt: created_at,
+        bidAmount: bid_amount,
+        auctionId: auction_id,
+      }),
+    );
   }
 }
