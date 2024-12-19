b28422e56e632f429f1ad819f40a9f99f0e73892
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-19 12:27:27 +0200

    update message dto

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,1 +1,10 @@
-export class CreateMessageDto {}
+import { IsUUID, IsString, Length } from 'class-validator';
+
+export class CreateMessageDto {
+  @IsUUID()
+  chatId: string;
+
+  @IsString()
+  @Length(1, 1000)
+  message: string;
+}
