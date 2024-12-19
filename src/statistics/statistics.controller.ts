8ce63641870b20782f41947ea95ed6734c9326b5
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-19 10:24:51 +0200

    + controller

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { StatisticsService } from './statistics.service';
+import { PaginationDto } from 'src/dto/pagination.dto';
 
 @Controller('statistics')
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
+
+  @Get('/numberOfCardInstances')
+  getNumberOfCardInstances(@Query() { page, take }: PaginationDto) {
+    return this.statisticsService.getNumberOfCardInstances(page, take);
+  }
+
+  @Get('/getTheNumberOfUsersPerSet')
+  getNumberOfUsersPerSet(@Query() { page, take }: PaginationDto) {
+    return this.statisticsService.getNumberOfUsersPerSet(page, take);
+  }
+
+  @Get('/cardsStatistics')
+  getCardsStatistics() {
+    return this.statisticsService.getTheMostAndTheLeastWidespreadCard();
+  }
 }
