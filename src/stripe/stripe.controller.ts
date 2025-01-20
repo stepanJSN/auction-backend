ff173a047c648127693880b3378ed9690798d929
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-20 15:58:48 +0200

    create payment intent endpoint and service method

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { StripeService } from './stripe.service';
+import { CreatePaymentIntentDto } from './dto/create-payment-intent.dto';
 
 @Controller('stripe')
 export class StripeController {
   constructor(private readonly stripeService: StripeService) {}
+
+  @Post('create-payment-intent')
+  async createPaymentIntent(
+    @Body() createPaymentIntentDto: CreatePaymentIntentDto,
+  ) {
+    return this.stripeService.createPaymentIntent(
+      createPaymentIntentDto.amount,
+    );
+  }
 }
--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+
+const EXCHANGE_RATE = 1.2;
 
 @Injectable()
-export class StripeService {}
+export class StripeService {
+  private readonly stripeKey: string;
+  private readonly stripe: Stripe;
+
+  constructor(private configService: ConfigService) {
+    this.stripeKey = this.configService.get<string>('stripe_key');
+    this.stripe = new Stripe(this.stripeKey);
+  }
+
+  async createPaymentIntent(numberOfPoints: number) {
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: numberOfPoints * EXCHANGE_RATE,
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    return {
+      clientSecret: paymentIntent.client_secret,
+    };
+  }
+}
