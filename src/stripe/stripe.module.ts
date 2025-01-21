27f66432c9d9a4d2bf62f7db528e9fe70cfbfd7c
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-21 15:54:13 +0200

    add the system module as a dependency for the stripe module

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { StripeController } from './stripe.controller';
 import { TransactionsModule } from 'src/transactions/transactions.module';
+import { SystemModule } from 'src/system/system.module';
 
 @Module({
-  imports: [TransactionsModule],
+  imports: [TransactionsModule, SystemModule],
   controllers: [StripeController],
   providers: [StripeService],
 })
