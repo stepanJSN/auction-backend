0d1002d1c24f8081e3de4b616e9afdb8d9b74486
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-23 15:44:35 +0200

    cards repository tests

--- config/mock-test-data.ts
+++ config/mock-test-data.ts
@@ -10,16 +10,16 @@ export const MOCK_AUCTION_ID = 'auctionId123';
 export const MOCK_ACCESS_TOKEN = 'accessToken';
 export const MOCK_REFRESH_TOKEN = 'refreshToken';
 export const MOCK_DATE = new Date();
-export const MOCK_URL = 'https://example.com';
+export const MOCK_IMAGE_URL = 'https://example.com/image.jpg';
 export const MOCK_CARD = {
   is_owned: false,
   id: MOCK_CARD_ID,
   created_at: MOCK_DATE,
   name: 'Test Card',
-  image_url: MOCK_URL,
+  image_url: MOCK_IMAGE_URL,
   location_id: 1,
   type: 'test-type',
   gender: Gender.female,
   is_active: true,
-  is_created_by_admin: false,
+  is_created_by_admin: true,
 };
